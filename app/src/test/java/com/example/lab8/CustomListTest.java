0ac9dc6b8546666174d2212f7f4b832466054d86
Fehintola Disu <fehintoladisu@gmail.com>
2024-10-24 20:17:54 -0600

    test case is failing for addCity

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -4,13 +4,24 @@ package com.example.lab8;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
-//import org.junit.Before;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
 import java.util.ArrayList;
 
 public class CustomListTest {
+    private CustomList list;
 
 
+    public CustomList createList(){
+        list = new CustomList(null, new ArrayList<>());
+        return list;
+    }
+
+    @Test
+    public void addCityTest(){
+        list = createList();
+        int listSize = list.getCount();
+        list.addCity(new City("Halifax", "NS"));
+        assertEquals(list.getCount(), listSize + 1);
+
+    }
 }
